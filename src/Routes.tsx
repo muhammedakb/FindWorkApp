bd4d46c56825cc05d0de01478c41b8cc8390ba94
muhammedakb <muhammetakb68@gmail.com>
2022-10-05 18:16:46 +0300

    Redux login operation added

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,23 +6,28 @@ import Jobs from "./pages/Jobs";
 import JobDetail from "./pages/JobDetail";
 import { RootDrawerParamList } from "./types/navigateTypes";
 import Login from "./pages/Login";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const Drawer = createDrawerNavigator<RootDrawerParamList>();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
-          headerTitleAlign: "center",
-          headerTintColor: "#40dac6",
-        }}
-      >
-        <Drawer.Screen name="Login" component={Login} />
-        <Drawer.Screen name="Jobs" component={Jobs} />
-        <Drawer.Screen name="FavoritedJobs" component={JobDetail} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      {/* TODO: Protected route */}
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{
+            headerTitleAlign: "center",
+            headerTintColor: "#40dac6",
+          }}
+        >
+          <Drawer.Screen name="Login" component={Login} />
+          <Drawer.Screen name="Jobs" component={Jobs} />
+          <Drawer.Screen name="FavoritedJobs" component={JobDetail} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 export default App;
